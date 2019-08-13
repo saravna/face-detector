621137cd979595f9462f75bf9c5cf73c699ae285
saravana kumar <ravi.saravna8@gmail.com>
2019-08-13 18:32:02 +0530

    Updates

---
+++ precache-manifest.9c9caf17817c30889a1aa9a854a6b677.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc52b88323c4619daad248d6663cec38",
+    "url": "/face-detector/index.html"
+  },
+  {
+    "revision": "f3ba595d02dd3d6d00a0",
+    "url": "/face-detector/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2227cc27aa7443ce5812",
+    "url": "/face-detector/static/css/main.cba0d442.chunk.css"
+  },
+  {
+    "revision": "f3ba595d02dd3d6d00a0",
+    "url": "/face-detector/static/js/2.245ccedc.chunk.js"
+  },
+  {
+    "revision": "2227cc27aa7443ce5812",
+    "url": "/face-detector/static/js/main.f2383f37.chunk.js"
+  },
+  {
+    "revision": "8231c30bf7d1441ac12b",
+    "url": "/face-detector/static/js/runtime~main.cbff86a3.js"
+  },
+  {
+    "revision": "78a3f79dadc5c38759c87c5a3e3e68c7",
+    "url": "/face-detector/static/media/Logo.78a3f79d.png"
+  }
+]);
