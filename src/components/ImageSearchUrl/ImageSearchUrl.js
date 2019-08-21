99da285992319da5eb0f32c364e01749742c923e
saravana kumar <ravi.saravna8@gmail.com>
2019-08-21 21:39:18 +0530

    change

--- src/components/ImageSearchUrl/ImageSearchUrl.js
+++ src/components/ImageSearchUrl/ImageSearchUrl.js
@@ -3,9 +3,9 @@ import React from 'react';
 const ImageSearchUrl = ({onSubmitClick, onInputChange}) => {
     return(
         <div>
-            <div className="br2 shadow-2 w-30 center pa3 tc">
-                <input type='text' onChange={onInputChange} className="center w-70"></input>
-                <button onClick={onSubmitClick} className="ba w-30 br1 link pointer grow bg-orange">Detect Face</button>
+            <div className="br2 shadow-2 w-30 center pa3 tc" style={{minWidth:'300px'}}>
+                <input type='text' onChange={onInputChange} className="center w-70" style={{minWidth:'150px'}}></input>
+                <button onClick={onSubmitClick} className="ba w-30 br1 link pointer grow bg-orange" style={{minWidth:'100px'}}>Detect Face</button>
             </div>
         </div>
     );
